db8a33de61cf85fc3750bd2fe19400038e2a3214
sean doyle <gigafud@gmail.com>
2019-04-03 22:59:20 +1100

    Updates

---
+++ precache-manifest.c8a74ebaddd79a449f834f4946c1dd14.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7487b148cb5350e0ceeaff548167b00",
+    "url": "/code-challenge-host/static/media/1002.c7487b14.jpg"
+  },
+  {
+    "revision": "de9d018f73f37a193edd",
+    "url": "/code-challenge-host/static/css/main.53576633.chunk.css"
+  },
+  {
+    "revision": "f18d0e980271d00ace2b",
+    "url": "/code-challenge-host/static/js/runtime~main.dd4f6219.js"
+  },
+  {
+    "revision": "2ff69a73e3e06403c6e9",
+    "url": "/code-challenge-host/static/js/2.c38ad585.chunk.js"
+  },
+  {
+    "revision": "305266b43a99dcc5170b7094d11351be",
+    "url": "/code-challenge-host/static/media/adrenalin.305266b4.svg"
+  },
+  {
+    "revision": "1539fa5748edf59cbf232e3ea435fda6",
+    "url": "/code-challenge-host/static/media/1001.1539fa57.jpg"
+  },
+  {
+    "revision": "1534c150914568ff0f7e8e7c2447c1dd",
+    "url": "/code-challenge-host/static/media/1001_thumb.1534c150.jpg"
+  },
+  {
+    "revision": "de9d018f73f37a193edd",
+    "url": "/code-challenge-host/static/js/main.feca2317.chunk.js"
+  },
+  {
+    "revision": "65ce0840d53e6b4b27c1b7603330ebfa",
+    "url": "/code-challenge-host/static/media/1002_thumb.65ce0840.jpg"
+  },
+  {
+    "revision": "40c062a03df6761cb65b7f62c27e9be1",
+    "url": "/code-challenge-host/static/media/1003.40c062a0.jpg"
+  },
+  {
+    "revision": "ead585afa0850bcac85a8cc734b47dac",
+    "url": "/code-challenge-host/static/media/1003_thumb.ead585af.jpg"
+  },
+  {
+    "revision": "806eda6eab77091d79cdb260e7631855",
+    "url": "/code-challenge-host/static/media/1004.806eda6e.jpg"
+  },
+  {
+    "revision": "2f12a9783a2ae6d1d320708904650079",
+    "url": "/code-challenge-host/static/media/1004_thumb.2f12a978.jpg"
+  },
+  {
+    "revision": "f672478adcaeb12c7a3e204c99b100d9",
+    "url": "/code-challenge-host/index.html"
+  }
+];
